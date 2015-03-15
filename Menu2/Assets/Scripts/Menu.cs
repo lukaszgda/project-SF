4bdeb75f44e71647ba09b8c097cef32598774945
OlejPL <olej15@gmail.com>
2015-03-15 21:42:47 +0100

    Menu
    Pierwsza wersja menu

---
+++ Menu2/Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public void LadujScene(int poziom)
+	{
+		Application.LoadLevel (poziom);
+	}
+
+	public void Wyjdz()
+	{
+		Application.Quit ();
+	}
+}
